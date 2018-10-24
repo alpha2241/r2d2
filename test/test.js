fe8fea777decb143bdef0797d30a22d8435a6e47
Vbansal1 <gaurav.verma@moonraft.com>
2018-10-25 01:07:44 +0530

    first commit

---
+++ test/test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe("simple test",()=>{
+    context("inside first",()=>{
+
+        it("test something",()=>{
+            expect(2).to.equal(2)
+        })
+
+        it("test 2",()=>{
+            expect({name : "Foo"}).to.deep.equal({name : "foo"})
+        })
+
+
+    })
+})
