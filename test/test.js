1dd114b91af0f8bf55351108d5fe212dbec07fa9
Gaurav  Verma <gkvom@outlook.com>
2018-10-25 01:20:11 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -13,5 +13,32 @@ describe("simple test",()=>{
         })
 
 
+        it("test something",()=>{
+            expect(2).to.equal(2)
+        })
+
+        it("test 2",()=>{
+            expect({name : "Foo"}).to.deep.equal({name : "foo"})
+        })
+
+
+        it("test something",()=>{
+            expect(2).to.equal(2)
+        })
+
+        it("test 2",()=>{
+            expect({name : "Foo"}).to.deep.equal({name : "foo"})
+        })
+
+
+        it("test something",()=>{
+            expect(2).to.equal(2)
+        })
+
+        it("test 2",()=>{
+            expect({name : "Foo"}).to.deep.equal({name : "foo"})
+        })
+
+
     })
 })
